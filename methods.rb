9c05bfcff9b5ef6d08371a933102917f767c779e
Eric Song <ericksong91@gmail.com>
2022-12-04 16:58:42 -0500

    Done with assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+
+def greet_programmer
+    print "Hello, programmer!"
+end
+  
+def greet(name)
+    print "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    print "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    return number / 2
+end
+
